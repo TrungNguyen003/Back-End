bbc71b0a61dfccec08ae9ec7ca3378d6a841782f
TrungNguyen003 <trungnt.114010121040@vtc.edu.vn>
2024-10-22 11:03:22 +0700

    P-R-J

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-  database: "mongodb+srv://trungzami:zami003@cluster0.gmjehle.mongodb.net/shoppets",
+    // Thêm tlsAllowInvalidCertificates=true chỉ khi cần thiết
+    database: "mongodb+srv://trungzami:zami003@cluster0.gmjehle.mongodb.net/shoppets?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true",
 };
